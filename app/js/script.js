20786d7c01359fd408865b81ad558e4c6e36aebd
Oliwia <Oliwia.9812@gmail.com>
2022-04-15 15:05:20 +0200

    added check cards functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,95 +1,100 @@
-const board = document.querySelector('.memory__board');
-const levelModal = document.querySelector('.modal');
-const easyLevelCardsImg = ['code', 'code', 'house', 'house', 'mug', 'mug', 'music', 'music'];
-const normalLevelCardsImg = ['code', 'code', 'crown', 'crown', 'fly', 'fly', 'house', 'house', 'mug', 'mug', 'music', 'music'];
-const hardLevelCardsImg = ['car', 'car', 'cloud', 'cloud', 'code', 'code', 'crown', 'crown', 'fly', 'fly', 'house', 'house', 'mug', 'mug', 'music', 'music'];
+const cardsImg = ['car', 'car', 'cloud', 'cloud', 'code', 'code', 'crown', 'crown', 'fly', 'fly', 'house', 'house', 'mug', 'mug', 'music', 'music'];
+const easyLevelCardsImg = cardsImg.slice(0,8);
+const normalLevelCardsImg = cardsImg.slice(0,12);
+const hardLevelCardsImg = [...cardsImg];
+const gameBoard = document.querySelector('.memory__board');
+let level;
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    setLevel();
+})
 
 const setLevel = () => {
     const levelButtons = document.querySelectorAll('.modal__button');
+
     levelButtons.forEach(button=>{
         button.addEventListener('click', (e) => {
-            const buttonLabel = e.currentTarget.innerHTML;
-          
-            if (buttonLabel.toLowerCase() === 'easy') {
-                displayLevel('easy', 8)
-            } else if (buttonLabel.toLowerCase() === 'normal') {
-                displayLevel('normal', 12)
-            } else if ( buttonLabel.toLowerCase() === 'hard') {
-                displayLevel('hard', 16)
-            }
+            level = e.currentTarget.innerHTML;
+            createBoard(level);
+            hideModal();
         });
     });
 }
 
-const displayLevel = (level, cardsNum) => {
-    board.classList.add(level);
-    clearBoard();
-    createGameBoard(cardsNum);
-    levelModal.classList.add('hide');
-}
-
-const clearBoard = () => {
-    board.replaceChildren();
-}
-
-const createGameBoard = (lvl) => {
-    const cards = [];
-    const cardsBack = [];
-    
-    for (var i = 0; i < lvl; i++) {
-        const card = document.createElement('div');
-        const cardInner = document.createElement('div');
-        const cardFront = document.createElement('div');
-        const cardBack = document.createElement('div');
-
-        card.classList.add('memory__card');
-        cardInner.classList.add('memory__card-inner');
-        cardFront.classList.add('memory__card-front');
-        cardBack.classList.add('memory__card-back');
-       
-        cardFront.style.backgroundImage = "url('app/img/question-icon.svg')";
-    
-        cardInner.append(cardBack, cardFront);
-        card.append(cardInner);
-        cards.push(card);
-        cardsBack.push(cardBack);
+const createBoard = (gameLevel) => {
+    switch (gameLevel.toLowerCase()) {
+         case 'easy':
+            createCards(easyLevelCardsImg)
+            break;
+        case 'normal':
+            createCards(normalLevelCardsImg)
+            break;    
+        case 'hard':
+            createCards(hardLevelCardsImg)
+            break; 
+        default:
+            console.log('Something went wrong. Please try again!')
+            break;
     }
 
-    cards.forEach(card => { 
-        board.append(card);
+    gameBoard.classList.add(gameLevel.toLowerCase());
+}
+
+const hideModal = () => {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('hide');
+}
+
+const createCards = (cardsData) => {
+    cardsData.sort(() => Math.random() - 0.5);
+    
+    cardsData.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('memory__card');
+        card.setAttribute('id', item);
+        
+        const cardInner = document.createElement('div');
+        cardInner.classList.add('memory__card-inner');
+
+        const cardFront = document.createElement('div');
+        cardFront.classList.add('memory__card-front', level.toLowerCase()); 
+        cardFront.style.backgroundImage = "url('app/img/question-icon.svg')";
+
+        const cardBack = document.createElement('div')
+        cardBack.classList.add('memory__card-back');
+        cardBack.style.backgroundImage = `url('app/img/${item}.svg')`;
+
+        cardInner.append(cardFront, cardBack);
+        card.append(cardInner);
+        gameBoard.append(card);
 
         card.addEventListener('click', () => {
-            card.classList.toggle('active');
-        });
-    });
-    addImageToCardBack(cardsBack, lvl);
+            card.classList.toggle('toggle');
+            checkCards(card);
+        })
+    })
 }
 
-
-const addImageToCardBack = (cards, cardsNum) => {
-    let level;
-
-    switch (cardsNum) {
-        case 8: 
-            level = easyLevelCardsImg;
-            break;
-        case 12: 
-            level = normalLevelCardsImg;
-            break;
-        case 16: 
-            level = hardLevelCardsImg;
-            break;
-        default:
-            alert('Something went wrong. Please try again!')
-    }
-
-    let shuffledCards = level.sort((a,b) => 0.5 - Math.random());
-
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].style.backgroundImage = `url('app/img/${shuffledCards[i]}.svg')`;
+const checkCards = (clickedCard) => {
+    clickedCard.classList.add('clicked');
+    const clickedCards = document.querySelectorAll('.clicked');
+   
+    if(clickedCards.length === 2) {
+        if(clickedCards[0].getAttribute('id')===clickedCards[1].getAttribute('id')){
+            clickedCards.forEach(card => {
+                card.classList.remove('clicked'); 
+                card.style.pointerEvents = 'none';
+            })
+        } else {
+            clickedCards.forEach(card => {
+               card.classList.remove('clicked');
+               setTimeout(()=>{card.classList.remove('toggle')},1000)
+               
+           })
+        }
     }
 }
 
 
-setLevel();
+
